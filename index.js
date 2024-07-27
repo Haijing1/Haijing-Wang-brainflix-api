cd6a82906b96fecbc5448f1a3d79672ba46de357
Haijing <Haijing.wang@hotmail.com>
2024-07-26 20:49:23 -0700

    add an array of video objects into /videos route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import videoIdRouter from "./routes/videos.js"
+import fs from "fs";
 
 const app = express();
 const PORT = 8080;
@@ -7,7 +8,13 @@ const PORT = 8080;
 app.use("/videos/videoId", videoIdRouter);
 
 app.get("/videos", (_req, res) => {
-    res.send("Welcome");
+    const videosBuffer = fs.readFileSync("./data/videos.json");
+    const videos = JSON.parse(videosBuffer);
+    const updatedVideos = videos.map((video) => {
+        return { id: video.id, title: video.title, channel: video.channel, image: video.image };
+    });
+    res.send(updatedVideos);
+    // res.send("Welcome");
 });
 
 app.listen(PORT, () => {
--- routes/videos.js
+++ routes/videos.js
@@ -1,9 +1,16 @@
+// import exporess and initialize router
 import express from "express"
-
 const router = express.Router();
 
+import fs from "fs";
+
+
 router.get("/", (req, res) => {
-    res.send("all videos");
+    // const videosBuffer = fs.readFileSync("./data/videos.json");
+
+    // res.send(videosBuffer);
+
+    res.send("Welcome");
 });
 
 export default router;
